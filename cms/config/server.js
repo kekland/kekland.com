ac0d7fef49bbb8db87f99f25fd18121f4147c6cc
kekland <kk.erzhan@gmail.com>
2021-08-04 13:25:17 +0600

    impl: add strapi

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bbca155a2dd0b4378b4f56292c508f78'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '563d58d1-219e-4cb1-a979-0dca40144fbb'
+};
