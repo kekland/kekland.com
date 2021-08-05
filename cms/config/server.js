7c222893b140d6ac873e35fe553ac8f3c816041b
kekland <kk.erzhan@gmail.com>
2021-08-05 18:31:49 +0600

    fix: add url to strapi config

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://cms.kekland.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'bbca155a2dd0b4378b4f56292c508f78'),
