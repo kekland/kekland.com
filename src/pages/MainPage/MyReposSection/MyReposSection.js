b29e8ca80a7cd0afc05da40c801075890c4d3e69
kekland <kk.erzhan@gmail.com>
2021-08-06 15:49:55 +0600

    fix: make repo colors more legible

--- src/pages/MainPage/MyReposSection/MyReposSection.js
+++ src/pages/MainPage/MyReposSection/MyReposSection.js
@@ -5,17 +5,27 @@ import { loadRepos } from '../../../api/api'
 import { Card, ShimmerCard } from '../../../components/Card/Card'
 import { ReactComponent as StarIcon } from '../../../icons/star-outline.svg'
 import { FlyInAnimation } from '../../../components/FlyInAnimation/FlyInAnimation'
+import { useSelector } from 'react-redux'
 
 const RepoCard = ({ data }) => {
+  const theme = useSelector((state) => state.theme.value)
+
   return (
     <FlyInAnimation>
       <a target="_blank" href={data.url}>
         <Card title={data.name} description={data.description}>
           <div style={{ height: 8 }} />
           <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <div style={{ width: 12, height: 12, backgroundColor: data.color, borderRadius: 6 }} />
-            <div style={{ width: 6 }} />
-            <span style={{ color: data.color, fontSize: 12 }}>{data.language}</span>
+            <div style={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+              filter: theme == 'light' ? 'contrast(90%) brightness(85%)' : '',
+            }}>
+              <div style={{ width: 12, height: 12, backgroundColor: data.color, borderRadius: 6 }} />
+              <div style={{ width: 6 }} />
+              <span style={{ color: data.color, fontSize: 12 }}>{data.language}</span>
+            </div>
             <div style={{ width: 24 }} />
             <StarIcon style={{ width: 14, opacity: 0.5 }} />
             <div style={{ width: 6 }} />
