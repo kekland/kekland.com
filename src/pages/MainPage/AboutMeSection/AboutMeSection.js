702ce6feb1ffc6fa1acb8eb640aa7e9176d516e7
kekland <kk.erzhan@gmail.com>
2021-08-11 17:16:09 +0600

    impl: change theming

--- src/pages/MainPage/AboutMeSection/AboutMeSection.js
+++ src/pages/MainPage/AboutMeSection/AboutMeSection.js
@@ -43,7 +43,7 @@ export const AboutMeSection = ({ titleRef }) => {
             </div>
           }
         </FlyInAnimation>
-        <FlyInAnimation>
+        <FlyInAnimation delay={100}>
           {
             data?.aboutMe ?
               <ReactMarkdown className='about-me-text'>
